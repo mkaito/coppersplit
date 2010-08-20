ba713ed33c6bc27afe7abd1b286c87387ebf82dc
Kaito Michishige <chris.webstar@gmail.com>
2010-08-20 17:04:18 +0100

    Changed script to use Dojo instead of jQuery.

--- js/plugins.js
+++ js/plugins.js
@@ -1,35 +1,113 @@
+// Array search helper
+Array.prototype.include = function(o) {
+    for (var i = 0; i < this.length; i++)
+    if (this[i] === o)
+    return true;
+    return false;
+}
 
+// Rounding helper. Will round down to the next 0.5 split below.
+function roundHalf(plat) {
+    if (!parseFloat(plat))
+    return 0;
 
-(function($){
+    var decimal = plat - parseInt(plat);
 
- 
+    if (decimal < 0.5)
+    return parseInt(plat);
+    else if (decimal >= 0.5)
+    return parseFloat(parseInt(plat)) + 0.5;
+}
 
+function splitPlat(plat, members, deductions) {
 
+    var debug = !!window.debug;
 
+    var result = {
+        'pool': 0
+    };
 
+    // Base split
+    var deductionPool = 0;
+    var even = plat / members;
+    result.even = even;
+    result.normal = members - deductions.length;
+    if (debug) {
+        log("Total members: " + members + ". Members with deductions: " + deductions.length + ". Normal members: " + result.normal + ".");
+    }
 
- 
+    // Parse deductions
+    result.deductions = [];
+    dojo.forEach(deductions,
+    function(line) {
 
+        var c = line.split(" ");
+        var out = {
+            'name': c[0],
+            'deduction': 0,
+            'plat': 0
+        };
 
+        // Calculate this member's deductions and add it to the total deduction pool
+        dojo.forEach(c,
+        function(deduction) {
+            if (parseInt(deduction)) {
+                out.deduction += parseInt(deduction);
+            }
+        });
 
-})(window.jQuery);
+        // Edge case: deduction > plat split.
+        out.plat = roundHalf((result.even + out.deduction));
+        if (out.plat < 0) {
+            out.plat = 0;
+            result.pool += result.even;
+        } else {
+            // Deduction is negative integer
+            result.pool -= out.deduction;
+        }
 
+        if (debug) {
+            log("Deductions for " + c[0] + ": " +
+            parseInt(out.deduction) + ", so he gets " +
+            roundHalf((result.even + out.deduction)) + "pp.");
 
+            log("Global deduction pool: " + result.pool);
+        }
 
-window.log = function(){
-  log.history = log.history || []; 
-  log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
-  }
+        result.deductions.push(out);
+    });
+
+    // Split the deduction pool among members without deductions
+    result.even = roundHalf(result.pool / result.normal + result.even);
+    if (debug)
+    log("There is a total deduction buffer of " + result.pool + ", which makes " + result.pool + "/" + result.normal + "=" + result.even + ".");
+
+    if (debug) {
+        var totalSplit = parseFloat(result.even) * parseFloat(result.normal);
+        dojo.forEach(result.deductions,
+        function(value) {
+            totalSplit += parseFloat(value.plat);
+        });
+        log("" + totalSplit + "pp used in the split, of " + plat + " that was available. We still have " + (plat - totalSplit) + " available.");
+    }
+
+    return result;
+}
+
+window.log = function() {
+    log.history = log.history || [];
+    log.history.push(arguments);
+    if (this.console) {
+        console.log(Array.prototype.slice.call(arguments));
+    }
 };
 
-(function(doc){
-  var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
-  };
+ (function(doc) {
+    var write = doc.write;
+    doc.write = function(q) {
+        log('document.write(): ', arguments);
+        if (/docwriteregexwhitelist/.test(q)) write.apply(doc, arguments);
+    };
 })(document);
 
 
--- js/script.js
+++ js/script.js
@@ -1,156 +1,78 @@
 /* Author: Michishige Kaito
 
 */
+if (dojo) {
+    d = dojo;
+    $ = d.query;
 
-// Just a handy array search method.
-Array.prototype.include = function(o) {
-  for(var i = 0; i < this.length; i++)
-	  if(this[i] === o)
-	    return true;
-  return false;
-}
+    dojo.addOnLoad(function() {
+        // Load URL configuration
+        var options = window.location.search.replace("?", "").split("&");
+        window.debug = options.include("debug");
+        window.test = options.include("test");
 
-// Rounding helper. Will round down to the next 0.5 split below.
-function roundHalf(plat) {
-  if( ! parseFloat(plat) )
-    return 0;
-    
-  var decimal = plat - parseInt(plat);
-    
-  if( decimal < 0.5 )
-    return parseInt( plat );
-  else if ( decimal >= 0.5 )
-    return parseFloat( parseInt( plat ) ) + 0.5;
-}
+        // Load settings from localStorage, if available.
+        if (Modernizr.localstorage) {
 
-function calculateSplit() {
+            if (! localStorage["coppersplit.help.not.seen"] ) {
+                log("Help pane hasn't been seen. Displaying.");
+                $("#help").style('display', 'block');
+            }
 
-  var result         = { 'pool': 0 };
+            if ( !! localStorage["coppersplit.data.plat"] ) {
+                if (debug)
+                    log("Loading platinum split data from localStorage");
+                $("input[name='plat']").val(parseFloat( localStorage["coppersplit.data.plat"] ));
+                $("textarea[name='deductions']").val( localStorage["coppersplit.data.deductions"] );
+                $("input[name='members']").val(parseInt( localStorage["coppersplit.data.members"] ));
+                fireSplit();
+            }
+        } // if Modernizr
+        
+        // Gather the data and display the calculations
+        var fireSplit = function() {
+            // Parse available coin and members
+            var plat = parseFloat($("input[name='plat']")[0].value);
+            var members = parseInt($("input[name='members']")[0].value);
+            var deductions = $("textarea[name='deductions']")[0].value.split("\n");
 
-  // Parse available coin and members
-  var platField      = $( "input[name='plat']"          );
-  var deductionField = $( "textarea[name='deductions']" );
-  var membersField   = $( "input[name='members']"       );
-  
-  var plat           = parseFloat( platField.val()  );
-  var members        = parseInt( membersField.val() );
-  var deductions     = deductionField.val().split( "\n" );
+            var result = splitPlat(plat, members, deductions);
 
-  // Base split
-  var deductionPool  = 0;
-  var even           = plat / members;
-  result.even        = even;
-  result.normal      = members - deductions.length;
-  if (debug) { log("Total members: " + members + ". Members with deductions: " + deductions.length + ". Normal members: " + result.normal + "."); }
+            // Parse output
+            output = "<h1>Calculonicus!</h1><h2>Have been naughty...</h2><ul>";
+            d.forEach(result.deductions, function(value) {
+                output += "<li>" + value.name + " <span class='number'>" + value.plat + "</span>pp (" + value.deduction + ").</li>";
+            });
+            output += "</ul>";
 
-  // Parse deductions
-  result.deductions  = [];
-  $.each( deductions, function(index, line) {
+            output += "<h2>And everyone else gets...</h2><p><span class='number'>" + result.even + "</span>pp!</p>";
 
-  	var c = line.split(" ");
-  	var out = {'name': c[0], 'deduction': 0, 'plat': 0};
-  	
-  	// Calculate this member's deductions and add it to the total deduction pool
-  	$.each( c, function(index, deduction) {
-  	    if ( parseInt( deduction ) ) {
-  		    out.deduction += parseInt(deduction);
-  	    }
-  	});
-  	out.deduction *= -1
-  
-  	// Edge case: deduction > plat split.
-  	out.plat = roundHalf(( result.even - out.deduction ));
-  	if(out.plat < 0) {
-  	  out.plat = 0;
-  	  result.pool += result.even;
-  	} else {
-  	  result.pool += out.deduction;
-  	}
-  
-  	if(debug) {
-  	  log("Deductions for " + c[0] + ": " +
-  		parseInt(out.deduction) + ", so he gets " +
-  		roundHalf((result.even + out.deduction)) + "pp.");
-  	
-  	  log("Global deduction pool: " + result.pool);
-  	}
-  
-  	result.deductions.push(out);
-  });
-  
-  // Split the deduction pool among members without deductions
-  result.even = roundHalf( result.pool / result.normal + result.even );
-  if(debug) 
-    log("There is a total deduction buffer of " + result.pool + ", which makes " + result.pool + "/" + result.normal + "=" + result.even + ".");
-  
-  if(debug) { 
-    var totalSplit = parseFloat(result.even) * parseFloat(result.normal);
-    $.each(result.deductions, function( index, value) {
-      totalSplit += parseFloat(value.plat);
+            if (debug)
+                log("" + result.deductions.length + " members with deductions.");
+
+            $("#results")[0].innerHTML = output;
+
+            // Store data in localStorage
+            if (Modernizr.localstorage) {
+                localStorage["coppersplit.data.plat"] = plat;
+                localStorage["coppersplit.data.deductions"] = deductions;
+                localStorage["coppersplit.data.members"] = members;
+            }
+        }; // fireSplit
+        
+        // Hook up events to fire the calculation
+        $("input, textarea").onblur(fireSplit).onclick(fireSplit);
+        $("form").onsubmit(function(event) {
+            dojo.stopEvent(event);
+            fireSplit();
+        });
+
+        // Dismiss the top help, store dismissal to localStorage
+        $("#help a").onclick(function(event) {
+            dojo.stopEvent(event);
+            if (Modernizr.localstorage)
+                localStorage["coppersplit.help.not.seen"] = false;
+            $("#help").style('display', 'none');
+        });
     });
-    log("" + totalSplit + "pp used in the split, of " + plat + " that was available. We still have " + ( plat - totalSplit ) + " available.");
-  }
-
-  // Parse output
-  output = "<h1>Calculonicus!</h1><h2>Members with deductions</h2><ul>";
-  $.each(result.deductions, function( index, value ) {
-	  output += "<li>" + value.name + " <span class='number'>" + value.plat + "</span>pp (-" + value.deduction + ").</li>";
-  });
-  output += "</ul>";
-  
-  output += "<h2>And everyone else gets...</h2><p><span class='number'>" + result.even + "</span>pp!</p>";
-
-  if(debug)
-	  log( "" + result.deductions.length + " members with deductions.");
-
-  $("#results").addClass("output").html(output);
-  
-  // Store data in localStorage
-  if( Modernizr.localstorage) {
-    localStorage["coppersplit.data.plat"] = platField.val();
-    localStorage["coppersplit.data.deductions"] = deductionField.val();
-    localStorage["coppersplit.data.members"] = membersField.val();
-  }
 }
-
-$(function() {
-  // Load URL configuration
-  var options = window.location.search.replace("?", "").split("&");
-  window.debug = options.include("debug");
-  window.test = options.include("test");
-    
-  // Load settings from localStorage, if available.
-  if(Modernizr.localstorage) {
-
-	if( localStorage.getItem( "coppersplit.help.dismissed" ) == "true" ) {
-	  log( "Loading help pane status from localStorage" );
-	  $( "#help" ).hide();
-	}
-
-	if( !! localStorage.getItem( "coppersplit.data.plat" ) ) {
-	  if(debug)
-	    log( "Loading platinum split data from localStorage" );
-    $( "input[name='plat']" ).val(parseFloat( localStorage.getItem( "coppersplit.data.plat" ) ) );
-    $( "textarea[name='deductions']" ).val( localStorage.getItem( "coppersplit.data.deductions" ) );
-    $( "input[name='members']" ).val(parseInt( localStorage.getItem( "coppersplit.data.members" ) ) );
-    calculateSplit();
-	  }
-  }
-
-  // Bind the split calculation
-  $("input[type='text']").blur(calculateSplit);
-  $("textarea").blur(calculateSplit);
-  $("form").submit(function(event) {
-    event.preventDefault();
-    calculateSplit();
-  });
-
-  // Dismiss the top help, store dismissal to localStorage
-  $("#help a").click(function(e) {
-	  e.preventDefault();
-	  if(Modernizr.localstorage)
-	    localStorage.setItem("coppersplit.help.dismissed", true);
-	  $("#help").hide();
-  });
-
-});
